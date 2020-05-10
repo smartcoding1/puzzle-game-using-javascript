bca876ee5d9c61b20e236a7114b0a2f065d260bc
shahnawazshaikh123 <32239343+shahnawazshaikh123@users.noreply.github.com>
2020-05-10 10:51:30 +0530

    Game bugs fixed and now have some improvement
    Fixed shuffling of Image
    Imrpovement of no of trial to play game, where i have decided to loose or Won.
    Some CSS and HTML is also chnaged for better implementation.

--- dummy.js
+++ dummy.js
@@ -1,9 +1,26 @@
-let random=Math.floor(Math.random()*9+1);
-let randompic=`${random}.jpg`;
+// let random=Math.floor(Math.random()*9+1);
+// let randompic=`${random}.jpg`;
 
-for(let i=0;i<=9;i++)
-{
-document.getElementsByClassName("pic")[i].setAttribute("src",randompic);
-random=Math.floor(Math.random()*9+1);
-randompic=`${random}.jpg`;
-}
+// for(let i=0;i<=9;i++)
+// {
+// document.getElementsByClassName("pic")[i].setAttribute("src",randompic);
+// random=Math.floor(Math.random()*9+1);
+// randompic=`${random}.jpg`;
+// }
+
+
+// var images =document.getElementsByClassName("pic")
+// images.
+// alert(images)
+// images.addEventListener('mouseover', changeDefOver);
+// // images.addEventListener('mouseout', changeDefOut);
+
+// function changeDefOver(e) {
+//   // e.target.classList.toggle('opacity-toggle');
+
+//   alert('h')
+// }
+
+// // function changeDefOut(e) {
+// //   e.target.classList.toggle('opacity-toggle');
+// // }
